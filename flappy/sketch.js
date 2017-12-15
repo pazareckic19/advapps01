c6783d767e05d8159b643b1500e042df60a9b528
pazareckic19 <34275020+pazareckic19@users.noreply.github.com>
2017-12-15 09:07:03 -0500

    Add files via upload

---
+++ flappy/sketch.js
@@ -0,0 +1,150 @@
+var bird;
+var pipes = [];
+var score;
+function setup() {
+  createCanvas(400, 600);
+  bird = new Bird();
+  pipes.push(new Pipe());
+   new Audio('https://s3.amazonaws.com/pb_previews/331_be-epic/331_full_be-epic_0176_preview.mp3').play()
+var score = 0;
+}
+
+function draw() {
+  background(0);
+  textSize(35);
+  text("Score",150,150);
+  stroke(0);
+  
+ 
+
+  
+ 
+  
+
+  for (var i = pipes.length-1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)) {
+      console.log("HIT");
+      
+     
+      
+			
+    }
+
+
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  }
+
+  bird.update();
+  bird.show();
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+    
+  }
+	
+}
+ 
+
+
+function mousePressed() {
+	{
+    bird.up();
+ //console.log("SPACE");
+    
+  }
+}
+
+function Bird() {
+  this.y = height/2;
+  this.x = 64;
+
+  this.gravity = 0.6;
+  this.lift = -15;
+  this.velocity = 20
+
+  this.show = function() {
+    fill(0, 0, 150);
+    ellipse(this.x, this.y, 32, 32);
+		fill(0,0, 150);
+		ellipse(this.x, this.y, 20, 20);  
+  }
+
+  this.up = function() {
+    this.velocity += this.lift;
+  }
+
+  this.update = function() {
+    this.velocity += this.gravity;
+    this.velocity *= 0.9
+    this.y += this.velocity;
+
+    if (this.y > height) {
+      this.y = height;
+      this.velocity = 0;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+  }
+}
+
+
+function Pipe() {
+  this.top = random(height/2);
+  this.bottom = random(height/2);
+  this.x = width;
+  this.w = 20;
+  this.speed = 2;
+
+  this.highlight = false;
+
+  this.hits = function(bird) {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+  }
+var lives = 3;   
+  this.show = function() {
+    fill(0,200,0);
+    if (this.highlight) {
+      fill(255, 0, 0); lives = lives - 1 
+			
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height-this.bottom, this.w, this.bottom);
+		 
+		
+	   
+		
+		
+		
+  }
+
+  this.update = function() {
+    this.x -= this.speed;
+  }
+
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    
+    }
+  }
+
+
+}
+
+
